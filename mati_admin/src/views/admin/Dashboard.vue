e4bb57361c403371407a1c74ab69de9be4c779d0
Mateusz Ożóg <mateusz2345678@gmail.com>
2019-03-22 23:28:05 +0100

    Init router

---
+++ mati_admin/src/views/admin/Dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="admin-dashboard">
+
+        <p>
+            Welcome <strong>admin</strong>!
+        </p>
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'adminDashboard',
+        data() {
+            return {}
+        }
+    }
+
+</script>
